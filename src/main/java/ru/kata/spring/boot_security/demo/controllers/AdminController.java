fc6a7720e42c697c1b0e0cc33d9c0b1ccc2b5028
SpaniardRos <referatguru@yandex.ru>
2022-08-16 22:34:19 +0300

    fix

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -5,10 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.RoleService;
@@ -46,14 +43,7 @@ public class AdminController {
         return "/admin/all_users";
     }
 
-    @GetMapping("/user_info")
-    public String updateUser(@RequestParam("id") Integer id, Model model) {
-        User user = userService.findByID(id);
-        model.addAttribute("user", user);
-        return "admin/userInfo";
-    }
-
-    @GetMapping("/saveUser")
+    @PostMapping("/saveUser")
     public String saveUser(@ModelAttribute("user") User user, @RequestParam(value = "role")
     String admin) {
         boolean isAdmin = admin.equals("admin");
@@ -61,7 +51,7 @@ public class AdminController {
         return "redirect:/admin/all_users";
     }
     @GetMapping("/updateUser")
-    public String update_User(@ModelAttribute("user") User user, @RequestParam(value = "roles1")
+    public String updateUser(@ModelAttribute("user") User user, @RequestParam(value = "roles1")
     String admin) {
         userService.updateUser(user, admin);
         return "redirect:/admin/all_users";
